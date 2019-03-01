efbce262f5363927c2cd1f4bc1828b8bb0f412a9
Glenn Snyder <gsnyder@blackducksoftware.com>
2019-03-01 15:14:37 -0500

    removing docker pieces for now

--- build_tag_and_publish.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build the docker image, tag it, and push it to the Docker Cloud repository
-#
-TAGS=$1
-
-source ./docker_build_common.bash
-
-build_tag_and_publish sage.dockerfile sage "${TAGS}"
